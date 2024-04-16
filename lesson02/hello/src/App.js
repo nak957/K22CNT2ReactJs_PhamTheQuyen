7b60c1cf8033cf5ddadd16959e4b22997c56b6a9
PhamTheQuyen <thequyen700@gmail.com>
2024-04-16 14:29:42 +0700

    lesson03

---
+++ lesson02/hello/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Demo Jsx </h1>
+     <h3>well come to PTQ</h3>
+     <hr/>
+     <h3>
+      Phạm Thế Quyền <br/>
+      20
+     </h3>
+    </div>
+  );
+}
+
+export default App;
