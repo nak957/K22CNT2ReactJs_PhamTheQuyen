5cb344093527bb7b4d578f0e2d91436422b65288
nak957 <thequyen700@gmail.com>
2024-06-11 16:16:56 +0700

    lesson08ontap

---
+++ lesson08/onthi/src/App.js
@@ -0,0 +1,108 @@
+import "./App.css";
+import { React, useState } from "react";
+import PTQListTask from "./component/PTQListTask";
+import PTQAddEditAndDelete from "./component/PTQAddEditAndDelete";
+function App() {
+  // Mock data
+  const PTQ_listTasks = [
+    {
+      ptq_taskId: 2210900059,
+      ptq_taskName: "Phạm Thế Quyền",
+      ptq_level: "Small",
+    },
+    {
+      ptq_taskId: 1,
+      ptq_taskName: "Học lập trình frontend",
+      ptq_level: "Small",
+    },
+    {
+      ptq_taskId: 2,
+      ptq_taskName: "Học lập trình reactjs",
+      ptq_level: "Medium",
+    },
+    {
+      ptq_taskId: 3,
+      ptq_taskName: "Lập trình với Frontend - Reactjs",
+      ptq_level: "High",
+    },
+    {
+      ptq_taskId: 4,
+      ptq_taskName: "Lập trình Fullstack (PHP, Java, NetCore)",
+      ptq_level: "Small",
+    },
+  ];
+  // sử dụng hàm useState để lưu trữ trạng thái dữ liệu
+  const [ptqListTasks, setptqListTasks] = useState(PTQ_listTasks);
+
+  // tạo state dữ liệu cho form (add, edit)
+  // Đối tượng task
+  const ptqTaksObj = {
+    ptq_taskId: 0,
+    ptq_taskName: "NTU",
+    ptq_level: "Medium",
+  };
+  // Tạo state
+  const [ptqTask, setptqTask] = useState(ptqTaksObj); // dữ liệu trên form
+  // state đê phân biệt trạng thái là thêm mới hay sửa
+  const [ptqIsAddOrEdit, setptqIsAddOrEdit ] = useState(true); // mặc định ban đầu là form thêm mới
+
+  // Nhận dữ liệu khi sửa
+  const ptqHandleEdit = (param) => {
+    console.log("App - Edit:", param);
+    
+    setptqTask(param);
+    // Cập nhật trạng thái form là sửa
+    setptqIsAddOrEdit(false);
+  };
+
+  const ptqHandleSubmit = (ptqParam) => {
+    console.log("App:", ptqParam);
+    if(ptqIsAddOrEdit===true){ // trường hợp thêm mới dữ liệu
+      setptqListTasks((prev) => {
+        return [...prev, ptqParam];
+      });
+    }else{ 
+      for (let i = 0; i < ptqListTasks.length; i++) {
+          if(ptqListTasks[i].ptq_taskId == ptqParam.ptq_taskId){
+            ptqListTasks[i] = ptqParam;
+            break;
+          }
+      }
+      
+      setptqListTasks((prev) => {
+        return [...prev];
+      });
+    }
+  };
+
+ 
+  const ptqHandleDelete = (param)=>{
+    let ptqResult = ptqListTasks.filter(x=>x.ptq_taskId != param.ptq_taskId);
+    setptqListTasks(ptqResult);
+  }
+  return (
+    <div className="container border">
+      <h1>Phạm Thế Quyền - K22CNT2</h1>
+      <hr />
+      <div>
+        {/* Danh sách list task  */}
+        <PTQListTask
+          renderptqListTasks={ptqListTasks}
+          onptqTaskEdit={ptqHandleEdit}
+          onptqTaskDelete = {ptqHandleDelete}
+        />
+      </div>
+      <div>
+        <PTQAddEditAndDelete 
+            renderptqTask = {ptqTask}
+            renderptqIsAddOrEdit = {ptqIsAddOrEdit}
+            ptqOnSubmit={ptqHandleSubmit} />
+      </div>
+    </div>
+  );
+
+
+  
+}
+
+export default App;
---
+++ lesson08/onthi/src/component/PTQListTask.js
@@ -0,0 +1,54 @@
+import React from 'react'
+
+export default function PTQListTask({renderptqListTasks, onptqTaskEdit, onptqTaskDelete}) {
+    console.log(renderptqListTasks);
+    // Hàm xử lý khi sửa
+    const ptqHandleEdit = (param)=>{
+        console.log("Click edit:", param);
+        onptqTaskEdit(param) //Đẩy lên App thông qua props (ontdtdTaskEdit)
+    }
+    // Hàm xử lý khi xóa
+    const ptqHandleDelete = (param)=>{
+        if(window.confirm('Bạn có chắc chắn xóa không')){
+            onptqTaskDelete(param) // Đẩy dữ liệu xóa lên trên App.js
+        }
+    }
+    // render data
+    let ptqElementTask = renderptqListTasks.map((task, index)=>{
+        return (
+            <tr key={index}>
+                <td>{index+1}</td>
+                <td>{task.ptq_taskId}</td>
+                <td>{task.ptq_taskName}</td>
+                <td>{task.ptq_level}</td>
+                <td>
+                    <button className='btn btn-success'
+                        onClick={()=>ptqHandleEdit(task)}
+                        >sửa</button>    
+                    <button className='btn btn-danger'
+                    onClick={()=>ptqHandleDelete(task)} >
+                        xóa</button>    
+                </td>
+            </tr>
+        )
+    })
+  return (
+    <div>
+        <h2>Danh sách các nhiệm vụ</h2>
+        <table className='table table-bordered'>
+            <thead>
+                <tr>
+                    <th>STT</th>
+                    <th>Task Id</th>
+                    <th>Task Name</th>
+                    <th>Task Level</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                {ptqElementTask}
+            </tbody>
+        </table>
+    </div>
+  )
+}
